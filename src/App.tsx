5538d2a15a08f036dcbf25b9d48b68ce88b4b7c6
Richwin_ <77601299+AmalRichwin-AT@users.noreply.github.com>
2021-09-05 12:54:32 +0530

    made updates

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,121 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useEffect, useState, useMemo } from 'react';
+import { Pokemon, getAll, getByName } from './API';
 
-function App() {
+import './styles.css';
+
+interface PokemonWithPower extends Pokemon {
+  power: number;
+}
+
+const calculatePower = (pokemon: Pokemon) =>
+  pokemon.hp +
+  pokemon.attack +
+  pokemon.defense +
+  pokemon.special_attack +
+  pokemon.special_defense +
+  pokemon.speed;
+
+let tableRender = 0;
+
+const PokemonTable: React.FunctionComponent<{
+  pokemon: PokemonWithPower[];
+}> = ({ pokemon }) => {
+  console.log(`table Render = ${tableRender++}`);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <table>
+      <thead>
+        <tr>
+          <td>ID</td>
+          <td>Name</td>
+          <td>Type</td>
+          <td colSpan={6}>Stats</td>
+          <td>Power</td>
+        </tr>
+      </thead>
+      <tbody>
+        {pokemon.map((p) => (
+          <tr key={p.id}>
+            <td>{p.id}</td>
+            <td>{p.name}</td>
+            <td>{p.type.join(',')}</td>
+            <td>{p.hp}</td>
+            <td>{p.attack}</td>
+            <td>{p.defense}</td>
+            <td>{p.special_attack}</td>
+            <td>{p.special_defense}</td>
+            <td>{p.speed}</td>
+            <td>{p.power}</td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
+  );
+};
+
+let appRender = 0;
+
+const MemodPokemonTable = React.memo(PokemonTable);
+
+export default function App() {
+  console.log(`appRender = ${appRender++}`);
+
+  const [pokemon, setPokemon] = useState<Pokemon[]>([]);
+
+  const [search, setSearch] = useState<string>('');
+
+  const [threshold, setThreshold] = useState(0);
+
+  useEffect(() => {
+    getByName(search).then(setPokemon);
+    return () => {};
+  }, [search]);
+
+  const pokemonWithPower = React.useMemo(
+    () => pokemon.map((p) => ({ ...p, power: calculatePower(p) })),
+    [pokemon]
+  );
+
+  const onSetThreshold = React.useCallback(
+    (evt: React.ChangeEvent<HTMLInputElement>) => setThreshold(+evt.currentTarget.value),
+    []
+  );
+
+  const countOverThreshold = React.useMemo(
+    () => pokemonWithPower.filter((p) => p.power > threshold).length,
+    [pokemonWithPower, threshold]
+  );
+
+  const min = React.useMemo(
+    () => Math.min(...pokemonWithPower.map((p) => p.power)),
+    [pokemonWithPower]
+  );
+
+  const max = React.useMemo(
+    () => Math.max(...pokemonWithPower.map((p) => p.power)),
+    [pokemonWithPower]
+  );
+
+  const onSetSearch = React.useCallback(
+    (evt: React.ChangeEvent<HTMLInputElement>) => setSearch(evt.currentTarget.value),
+    []
+  );
+
+  return (
+    <div>
+      <div className="top-bar">
+        <div>Search</div>
+        <input type="text" value={search} onChange={onSetSearch} />
+        <div>Power threshold</div>
+        <input type="text" value={threshold} onChange={onSetThreshold}></input>
+        <div>Count over threshold: {countOverThreshold}</div>
+      </div>
+      <div className="two-column">
+        <MemodPokemonTable pokemon={pokemonWithPower} />
+        <div>
+          <div>Min: {min}</div>
+          <div>Max: {max}</div>
+        </div>
+      </div>
     </div>
   );
 }
-
-export default App;
